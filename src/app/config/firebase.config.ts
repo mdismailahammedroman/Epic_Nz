5ea3e14812dc7b548821a7656f4e0bc7ce6377c8
Md.Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2026-02-24 09:37:06 +0600

    firebase isseu

--- src/app/config/firebase.config.ts
+++ src/app/config/firebase.config.ts
@@ -1,40 +1,8 @@
-// import admin from "firebase-admin";
-// import fs from "fs";
-// import path from "path";
-// import { envVar } from "./envVar";
+import admin from "firebase-admin";
+import fs from "fs";
+import path from "path";
+import { envVar } from "./envVar";
 
-// let initialized = false;
-
-// export const initFirebase = () => {
-//   if (initialized) return;
-//   if (admin.apps.length) {
-//     initialized = true;
-//     return;
-//   }
-
-//   const p = envVar.FIREBASE_SERVICE_ACCOUNT_PATH;
-//   if (!p) throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_PATH");
-
-//   const resolved = path.resolve(process.cwd(), p);
-//   if (!fs.existsSync(resolved)) {
-//     throw new Error(`Firebase service account not found: ${resolved}`);
-//   }
-
-//   const serviceAccount = JSON.parse(fs.readFileSync(resolved, "utf8"));
-
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-
-//   initialized = true;
-// };
-
-// export const fcmMessaging = () => {
-//   initFirebase();
-//   return admin.messaging();
-// };
-import admin, { ServiceAccount } from "firebase-admin";
-import { envVar } from "./envVar"; // your env loader
 let initialized = false;
 
 export const initFirebase = () => {
@@ -44,18 +12,15 @@ export const initFirebase = () => {
     return;
   }
 
-  const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } =
-    envVar;
+  const p = envVar.FIREBASE_SERVICE_ACCOUNT_PATH;
+  if (!p) throw new Error("Missing FIREBASE_SERVICE_ACCOUNT_PATH");
 
-  if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
-    throw new Error("Missing Firebase environment variables");
+  const resolved = path.resolve(process.cwd(), p);
+  if (!fs.existsSync(resolved)) {
+    throw new Error(`Firebase service account not found: ${resolved}`);
   }
 
-  const serviceAccount: ServiceAccount = {
-    projectId: envVar.FIREBASE_PROJECT_ID,
-    clientEmail: envVar.FIREBASE_CLIENT_EMAIL,
-    privateKey: envVar.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  };
+  const serviceAccount = JSON.parse(fs.readFileSync(resolved, "utf8"));
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
